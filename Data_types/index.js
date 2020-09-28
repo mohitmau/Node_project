488af36e773f69b6b8cfa3e7bb4eb3522cd7c9d2
Mohit <maumhr12@gmail.com>
2020-09-28 09:06:50 +0545

    first commit

---
+++ Data_types/index.js
@@ -0,0 +1,20 @@
+//Declare variable and assign value to it
+let interger_value=1, float_value=0.5, string_value='hello world', bool_value = false, undefined_value;
+
+//Logging variable value through console.log()
+console.log("=======");
+console.log(interger_value);
+console.log(string_value);
+console.log(float_value);
+console.log(bool_value);
+console.log(undefined_value);
+console.log("=======");
+
+
+console.log("=======");
+console.log(typeof interger_value);
+console.log(typeof float_value);
+console.log(typeof string_value);
+console.log(typeof bool_value);
+console.log(typeof undefined_value);
+console.log("=======");
