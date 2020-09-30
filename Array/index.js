d7bc74979bc97baed5cdfc450868f327b792cdd5
Mohit <maumhr12@gmail.com>
2020-09-30 08:58:53 +0545

    array modified

--- Array/index.js
+++ Array/index.js
@@ -1,8 +1,35 @@
-// let fruit1 = 'apple';
-// let fruit2 ='mango', fruit3 = 'pears';
+// // let fruit1 = 'apple';
+// // let fruit2 ='mango', fruit3 = 'pears';
 
 // let fruit = ['apple', 'mango','pears'];
-let fruit = new Array ('apple', 'mango', 'pears');
+// let fruit = new Array ('apple', 'mango', 'pears', 1, true, null, [1,2]);
+
 // console.log(fruit1, fruit2, fruit3);
 
-console.log(fruit);
+// finding length
+// console.log(fruit.length);
+// console.log(fruit.indexOf('pears'));
+
+//finding through index
+// // console.log('===========');
+// // console.log(fruit);
+// // console.log(typeof fruit);
+// // console.log('===========');
+// // console.log(fruit[0]);
+// // console.log(typeof fruit[0]);
+// // console.log('===========');
+
+// array ko bhitra ko array khojne
+// console.log(fruit[6]); // [1,2]
+// console.log(typeof fruit[6]);
+// console.log(fruit[6][0])
+
+// exercise
+// let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+
+// console.log(newArr[2][2]);
+
+//push or pop
+let fruit = new Array ('apple', 'mango', 'pears', 1, true, null, [1,2]);
+fruit.push(['avacado','banana']);
+fruit[1]='guava';
