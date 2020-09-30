f460023eed202fec3a9235229b32d7b320a33ca1
Mohit <maumhr12@gmail.com>
2020-09-30 08:05:19 +0545

    added folder arrary and updated data_types

---
+++ Array/index.js
@@ -0,0 +1,8 @@
+// let fruit1 = 'apple';
+// let fruit2 ='mango', fruit3 = 'pears';
+
+// let fruit = ['apple', 'mango','pears'];
+let fruit = new Array ('apple', 'mango', 'pears');
+// console.log(fruit1, fruit2, fruit3);
+
+console.log(fruit);
--- Data_types/index.js
+++ Data_types/index.js
@@ -1,20 +1,32 @@
 //Declare variable and assign value to it
-let interger_value=1, float_value=0.5, string_value='hello world', bool_value = false, undefined_value;
+let interger_value=1, float_value=0.5, string_value='hello world', bool_value = false, null_value = null, undefined_value;
+const CONSTANT_VALUE= 'IIMS'
 
-//Logging variable value through console.log()
-console.log("=======");
+
+// //Logging variable value through console.log()
+// console.log("=======");
+// console.log(interger_value);
+// console.log(string_value);
+// console.log(float_value);
+// console.log(bool_value);
+// console.log(undefined_value);
+// console.log("=======");
+
+// //check data type of the variable
+// console.log("=======");
+// console.log(typeof interger_value);
+// console.log(typeof float_value);
+// console.log(typeof string_value);
+// console.log(typeof bool_value);
+// console.log(typeof undefined_value);
+// console.log("=======");
+
+
+//Change variable value
 console.log(interger_value);
-console.log(string_value);
-console.log(float_value);
-console.log(bool_value);
-console.log(undefined_value);
-console.log("=======");
-
-
-console.log("=======");
 console.log(typeof interger_value);
-console.log(typeof float_value);
-console.log(typeof string_value);
-console.log(typeof bool_value);
-console.log(typeof undefined_value);
-console.log("=======");
+interger_value='mohit';
+console.log(interger_value);
+console.log(typeof interger_value);
+
+console.log(CONSTANT_VALUE);
