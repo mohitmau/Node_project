fed715a39f06a971b76c5cf4b5a1ec2d715a2983
Mohit <maumhr12@gmail.com>
2020-11-01 12:34:53 +0545

    arrow_functions added

---
+++ arrow_functions/index.js
@@ -0,0 +1,17 @@
+// function helloWorld(){
+// 	console.log('helloWorld');
+// }
+
+// helloWorldArrow = (firstname) => firstname;
+
+// console.log(helloWorldArrow('hello'));
+
+
+
+// let multiplyES5 = function (x,y) {
+// 	return x * y;
+// };
+// 	console.log(multiplyES5(2,2));
+
+const multiplyES6 = (x,y) => x * y;
+	console.log(multiplyES6(2,2));
