81d87bbd0f1cdc95e57bc41df1aa1546a3faf3da
Mohit <maumhr12@gmail.com>
2020-11-01 12:37:24 +0545

    updated folders

--- Object/object.js
+++ Object/object.js
@@ -1,7 +1,9 @@
-let student = {};
+let student = {
 student.name='mohit';
 student.id=181120;
 student.gender='male';
 student.faculty='B.Sc.(Hons) Computing';
 student.address='Kirtipur';
-console.log(student);
+};
+console.log('My name is ' + student.name + '.I am the faculty of ' + 
+	student.faculty + ' from the ' + student.semester + '.I am the batch of ' + student.batch + '. My id is ' + student.id);
